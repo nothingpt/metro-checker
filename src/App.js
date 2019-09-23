a3d898b35d8913e2df02fc9e2804a115a709d185
nunosantos <nunosantos@chinasystems.com>
2019-09-23 16:55:27 +0100

    refactor to allow search by station - no css

--- src/App.js
+++ src/App.js
@@ -6,238 +6,123 @@ import * as SVGLoaders from "svg-loaders-react";
 
 import "./App.css";
 import Station from "./Station";
+import {
+  STATIONS,
+  getLinha,
+  getStationByCode,
+  getStationById
+} from "./helpers/stations";
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
-function App() {
+function App(props) {
+  var id = 0;
+  const [code, updateCode] = useState('CG');
+  const [comboios, updateComboios] = useState([]);
   const [isLoading, updateIsLoading] = useState(true);
-  const [caisSodre, updateCaisSodre] = useState({});
-  const [telheiras, updateTelheiras] = useState({});
-  const [rato, updateRato] = useState({});
-  const [odivelas, updateOdivelas] = useState({});
-  const [stApolonia, updateStApolonia] = useState({});
-  const [reboleira, updateReboleira] = useState({});
 
   dotenv.config();
 
-  useEffect(() => {
-    async function getData() {
-      updateIsLoading(true);
-      const res = await axios.get(
-        "https://api.metrolisboa.pt:8243/estadoServicoML/1.0.0/tempoEspera/Estacao/CG",
-        {
-          headers: {
-            Authorization: "Bearer " + process.env.REACT_APP_API_KEY
-          }
+  async function getData(code = 'CG') {
+    updateIsLoading(true);
+
+    // load the upcoming trains for station CG (Campo Grande)
+    // Soon, it will load this or the station stored in local storage
+
+    const res = await axios.get(
+      `https://api.metrolisboa.pt:8243/estadoServicoML/1.0.0/tempoEspera/Estacao/${code}`,
+      {
+        headers: {
+          Authorization: "Bearer " + process.env.REACT_APP_API_KEY
         }
-      );
+      }
+    );
 
-      res.data.resposta.forEach(entry => {
-        const a = entry.hora.substr(0, 4);
-        const m = entry.hora.substr(4, 2);
-        const d = entry.hora.substr(6, 2);
-        const h = entry.hora.substr(8, 2);
-        const mn = entry.hora.substr(10, 2);
-        const s = entry.hora.substr(12, 2);
-        let hora = `${a}-${m}-${d}T${h}:${mn}:${s}`;
-        let tempo1, tempo2, tempo3;
+    const aComboios = res.data["resposta"].map(entry => {
+      const a = entry.hora.substr(0, 4);
+      const m = entry.hora.substr(4, 2);
+      const d = entry.hora.substr(6, 2);
+      const h = entry.hora.substr(8, 2);
+      const mn = entry.hora.substr(10, 2);
+      const s = entry.hora.substr(12, 2);
+      let hora = `${a}-${m}-${d}T${h}:${mn}:${s}`;
 
-        if ( !entry.tempoChegada1 || entry.tempoChegada1 == '0' || isNaN(entry.tempoChegada1)) {
-          tempo1 = '--:--:--';
-        } else {
-          tempo1 = moment(hora)
+      if (
+        !entry.tempoChegada1 ||
+        entry.tempoChegada1 === "0" ||
+        isNaN(entry.tempoChegada1)
+      ) {
+        entry.tempoChegada1 = "--:--:--";
+      } else {
+        entry.tempoChegada1 = moment(hora)
           .add(entry.tempoChegada1, "s")
           .format("HH:mm:ss");
-        }
+      }
 
-        if ( !entry.tempoChegada2 || entry.tempoChegada2 == '0' || isNaN(entry.tempoChegada2)) {
-          tempo2 = '--:--:--';
-        } else {
-          tempo2 = moment(hora)
+      if (
+        !entry.tempoChegada2 ||
+        entry.tempoChegada2 === "0" ||
+        isNaN(entry.tempoChegada2)
+      ) {
+        entry.tempoChegada2 = "--:--:--";
+      } else {
+        entry.tempoChegada2 = moment(hora)
           .add(entry.tempoChegada2, "s")
           .format("HH:mm:ss");
-        }
+      }
 
-        if ( !entry.tempoChegada3 || entry.tempoChegada3 == '0' || isNaN(entry.tempoChegada3)) {
-          tempo3 = '--:--:--';
-        } else {
-          tempo3 = moment(hora)
+      if (
+        !entry.tempoChegada3 ||
+        entry.tempoChegada3 === "0" ||
+        isNaN(entry.tempoChegada3)
+      ) {
+        entry.tempoChegada3 = "--:--:--";
+      } else {
+        entry.tempoChegada3 = moment(hora)
           .add(entry.tempoChegada3, "s")
           .format("HH:mm:ss");
-        }
+      }
 
-        switch (entry.destino) {
-          case "54": // Cais do Sodre
-            updateCaisSodre({
-              tempoChegada1: tempo1,
-              tempoChegada2: tempo2,
-              tempoChegada3: tempo3
-            });
-            break;
-          case "50": // Telheiras
-            updateTelheiras({
-              tempoChegada1: tempo1,
-              tempoChegada2: tempo2,
-              tempoChegada3: tempo3
-            });
-            break;
-          case "48": // Rato
-            updateRato({
-              tempoChegada1: tempo1,
-              tempoChegada2: tempo2,
-              tempoChegada3: tempo3
-            });
-            break;
-          case "43": // Odivelas
-            updateOdivelas({
-              tempoChegada1: tempo1,
-              tempoChegada2: tempo2,
-              tempoChegada3: tempo3
-            });
-            break;
-          default:
-            break;
-        }
-      });
+      entry.id = id++;
+      entry.linha = getLinha(code, entry.destino);
+      entry.destino = getStationById(entry.destino);
+      entry.fullName = getStationByCode(entry.stop_id);
 
-      const res2 = await axios.get(
-        "https://api.metrolisboa.pt:8243/estadoServicoML/1.0.0/tempoEspera/Estacao/AV",
-        {
-          headers: {
-            Authorization: "Bearer 8c9f863b-f8af-3592-ae31-c3e3e4d8eff1"
-          }
-        }
-      );
-
-      res2.data.resposta.forEach(entry => {
-        const a = entry.hora.substr(0, 4);
-        const m = entry.hora.substr(4, 2);
-        const d = entry.hora.substr(6, 2);
-        const h = entry.hora.substr(8, 2);
-        const mn = entry.hora.substr(10, 2);
-        const s = entry.hora.substr(12, 2);
-        var hora = `${a}-${m}-${d}T${h}:${mn}:${s}`;
-        let tempo1, tempo2, tempo3;
-
-        if ( !entry.tempoChegada1 || entry.tempoChegada1 == '0' || isNaN(entry.tempoChegada1)) {
-          tempo1 = '--:--:--';
-        } else {
-          tempo1 = moment(hora)
-          .add(entry.tempoChegada1, "s")
-          .format("HH:mm:ss");
-        }
-        if ( !entry.tempoChegada2 || entry.tempoChegada2 == '0' || isNaN(entry.tempoChegada2)) {
-          tempo2 = '--:--:--';
-        } else {
-          tempo2 = moment(hora)
-          .add(entry.tempoChegada2, "s")
-          .format("HH:mm:ss");
-        }
-        if ( !entry.tempoChegada3 || entry.tempoChegada3 == '0' || isNaN(entry.tempoChegada3)) {
-          tempo3 = '--:--:--';
-        } else {
-          tempo3 = moment(hora)
-          .add(entry.tempoChegada3, "s")
-          .format("HH:mm:ss");
-        }
-
-        switch (entry.destino) {
-          case "42": // Santa Apolonia
-            updateStApolonia({
-              tempoChegada1: tempo1,
-              tempoChegada2: tempo2,
-              tempoChegada3: tempo3
-            });
-            break;
-          case "33": // Reboleira
-            updateReboleira({
-              tempoChegada1: tempo1,
-              tempoChegada2: tempo2,
-              tempoChegada3: tempo3
-            });
-            break;
-          default:
-            break; 
-        }
-      });
       updateIsLoading(false);
-    }
+      return entry;
+    });
+
+    updateComboios(aComboios)
+  }
+
+  useEffect(() => {
     getData();
   }, []);
 
+  const handleChange = (e) => {
+    const code = e.target.value;
+    updateCode(code);
+    updateComboios([]);
+    getData(code);
+  }
+
   return (
-    <div className="global-container">
-      <div className="container">
-        <div className="line-container linha-azul">
-          <div className="line-blue">
-            <h5>Linha Azul</h5>
-          </div>
-          {isLoading ? (
-            <div className="svg">
-              <SVGLoaders.ThreeDots className='loader-blue' />
-            </div>
-          ) : (
-            <Station stationname="Santa Apolónia" trains={stApolonia} />
-          )}
-          {isLoading ? (
-            <div className="svg">
-              <SVGLoaders.ThreeDots className='loader-blue' />
-            </div>
-          ) : (
-            <Station
-              stationname="Reboleira"
-              trains={reboleira}
-              style={{ alignSelf: "center" }}
-            />
-          )}
+    <div>
+      <select value={code} onChange={ handleChange }>
+        <option value=""></option>
+        {STATIONS.map(station => (
+          <option value={station.code} key={station.code}>{station.name}</option>
+        ))}
+      </select>
+      {isLoading ? (
+        <div className="svg">
+          <SVGLoaders.ThreeDots className="loader-blue" />
         </div>
-        <div className="line-container linha-amarela">
-          <div className="line-yellow">
-            <h5>Linha Amarela</h5>
-          </div>
-          {isLoading ? (
-            <div className="svg">
-              <SVGLoaders.ThreeDots className='loader-yellow' />
-            </div>
-          ) : (
-            <Station stationname="Rato" trains={rato} />
-          )}
-          {isLoading ? (
-            <div className="svg">
-              <SVGLoaders.ThreeDots className='loader-yellow' />
-            </div>
-          ) : (
-            <Station
-              stationname="Odivelas"
-              trains={odivelas}
-              style={{ alignSelf: "center" }}
-            />
-          )}
-        </div>
-        <div className="line-container linha-verde">
-          <div className="line-green">
-            <h5>Linha Verde</h5>
-          </div>
-          {isLoading ? (
-            <div className="svg">
-              <SVGLoaders.ThreeDots className='loader-green' />
-            </div>
-          ) : (
-            <Station stationname="Cais do Sodré" trains={caisSodre} />
-          )}
-          {isLoading ? (
-            <div className="svg">
-              <SVGLoaders.ThreeDots className='loader-green' />
-            </div>
-          ) : (
-            <Station
-              stationname="Telheiras"
-              trains={telheiras}
-              style={{ alignSelf: "center" }}
-            />
-          )}
-        </div>
-      </div>
+      ) : (
+        comboios.map(comboio => (
+          <div key={comboio.id}>{comboio.destino ? <Station trains={comboio} /> : ""}</div>
+        ))
+      )}
     </div>
   );
 }
--- src/Station.js
+++ src/Station.js
@@ -3,12 +3,13 @@ import React from "react";
 const Station = props => {
   return (
     <div className="station" {...props}>
-      <div className="station-name">{props.stationname}</div>
+      <div className="station-name">{ props.trains.fullName }</div>
       <div className="upcoming-trains">
+        <div>{props.trains.destino} - Linha { props.trains.linha }</div>
         <ul>
-          <li>{props.trains.tempoChegada1}</li>
-          <li>{props.trains.tempoChegada2}</li>
-          <li>{props.trains.tempoChegada3}</li>
+          <li key={props.trains.tempoChegada1}>{props.trains.tempoChegada1}</li>
+          <li key={props.trains.tempoChegada2}>{props.trains.tempoChegada2}</li>
+          <li key={props.trains.tempoChegada3}>{props.trains.tempoChegada3}</li>
         </ul>
       </div>
     </div>
