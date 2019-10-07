4b72f3900c9bc86c0bbd8d9d77560a18272478e0
nunosantos <nunosantos@chinasystems.com>
2019-10-07 13:29:55 +0100

    The mobile has been styled. YAY

--- src/Station.js
+++ src/Station.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 const Station = props => {
+  const cssClasses = `upcoming-trains upcoming-trains__${props.trains.linha}`;
   return (
     <div className="station" {...props}>
       <div className={"upcoming-trains-title__" + props.trains.linha}>{props.trains.destino}</div>
-      <div className="upcoming-trains">
+      <div className={cssClasses}>
         <ul>
           <li key={props.trains.tempoChegada1}>{props.trains.tempoChegada1}</li>
           <li key={props.trains.tempoChegada2}>{props.trains.tempoChegada2}</li>
